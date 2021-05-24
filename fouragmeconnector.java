501e7e6bef72e85c065fe94296053b809938398f
anweshan2001 <53610465+anweshan2001@users.noreply.github.com>
2021-05-24 14:51:25 +0530

    Update fouragmeconnector.java

--- fouragmeconnector.java
+++ fouragmeconnector.java
@@ -1,12 +1,12 @@
 package fourgameconnector;
-import javax.swing.*;
+
 public class fouragmeconnector {
 
 	public static void main(String[] args) {
 		Board b=new Board();
        connect4logic game=new connect4logic("R","Y");
        game.startGame();
-      Connect4GUI g=new Connect4GUI();
+   
 
        
        
